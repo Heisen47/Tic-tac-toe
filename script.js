ad86233d1b62e9c20396881b726f2bceb4c9b45a
Heisen47 <Iamjeet47@gmail.com>
2023-05-08 11:56:46 +0530

    Changed the length of the webpage and made the gameboard

--- script.js
+++ script.js
@@ -1,8 +1,42 @@
-let choices = (()=>{
+// asking the player to select between X and O 
+
+let choices = (() => {
     let btn = document.querySelectorAll(".btn");
-    let btnChoice = btn.forEach(button => {
-        button.addEventListener('click',(e)=>{
-            console.log(e.target.textContent)
+    let btnChoice = null;
+
+    btn.forEach((button) => {
+        button.addEventListener('click', (e) => {
+            // return (e.target.textContent);
+            btnChoice = e.target.textContent;
+            console.log(e.target.textContent);
         })
     })
+})()
+
+
+//Making the gameboard which will print the array combinations
+
+let gameboard = (() => {
+    let Arr = [];
+    let cells = document.querySelectorAll(".cell");
+    let clickedIndex = null;
+    
+// this forEach loop will not let the array be more than 3 long 
+        cells.forEach((cell, index) => {
+        cell.addEventListener('click', () => {
+            clickedIndex = Arr.push(index);
+            if (clickedIndex > 3 ) {
+                return(Arr = []);
+            }
+
+            else{
+            console.log(Arr);
+            console.log(clickedIndex);
+            return(clickedIndex);
+        }
+        })        
+
+       
+    })
+
 })()
