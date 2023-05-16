966ad3655e1dd137dd7bdb7b0da04c43cc9cc346
Heisen47 <Iamjeet47@gmail.com>
2023-05-16 11:33:24 +0530

    The complete game logic with win and tie

--- app.js
+++ app.js
@@ -1,50 +1,74 @@
-console.log('welcome to tic-tac-toe');
+console.log('Welcome to Tic-Tac-Toe');
 
-let changeTurn = (() => {
-    let turn = () => 'X';
-    // console.log (turn);
+const gameboard = (() => {
+  const board = ["", "", "", "", "", "", "", "", ""];
 
-    return () => {
-        turn = turn === "X" ? "O" : "X";
-        return turn;
-    };
+  const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  let currentPlayer = "X";
+
+  const render = () => {
+    const cells = document.querySelectorAll('.cell');
+
+    cells.forEach((cell, index) => {
+      cell.addEventListener('click', () => {
+        if (cell.innerText === '') {
+          cell.innerText = currentPlayer;
+          board[index] = currentPlayer;
+
+          if (checkWin(currentPlayer)) {
+            alert(`${currentPlayer} wins!`);
+            resetBoard();
+          } else if (checkTie()) {
+            alert("It's a tie!");
+            resetBoard();
+          } else {
+            currentPlayer = currentPlayer === "X" ? "O" : "X";
+          }
+        }
+      });
+    });
+  };
+
+  const checkWin = (player) => {
+    return winningCombos.some((combo) => {
+      return combo.every((index) => {
+        return board[index] === player;
+      });
+    });
+  };
+
+  const checkTie = () => {
+    return board.every((cell) => {
+      return cell !== "";
+    });
+  };
+
+  const resetBoard = () => {
+    board.fill("");
+    currentPlayer = "X";
+
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach((cell) => {
+      cell.innerText = '';
+    });
+  };
+
+  return {
+    render,
+  };
 })();
 
-let render = () => {
-    let cells = document.querySelectorAll('.cell');
-    // cells.style.fontSize = "50px";
-    
-
-    let select = () => {
-
-        Array.from(cells).forEach((e) => {
-            e.addEventListener('click', () => {
-                if (e.innerText === '') {
-                    e.innerText = changeTurn();
-                    // checkWin();
-                }
-            })
-        })
-    };
-
-    select();
-    return { select, cells };
-}
-
-render();
-
-
-
-let gameboard = () => {
-    let XArr = [];
-    let OArr = [];
-    const {render} = cells().select();
-    console.log(print)
-}
-
-
-
-
+gameboard.render();
 
 
 
